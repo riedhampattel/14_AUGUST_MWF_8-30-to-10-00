f9f42243e009fb0d1fba4f54da2285bd96eadfc2
riedhampattel <riedhampattel.tops@gmail.com>
2024-09-18 09:24:49 +0530

    updated

---
+++ C Programming/Loops/13.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	
+	for(i=1;i<=10;i++)
+	{	
+		if(i%3==0)
+		{
+			continue;
+		}
+		printf("\n%d",i);
+	}
+	
+	return 0;
+}
--- C Programming/Loops/p10.c
+++ C Programming/Loops/p10.c
@@ -1,41 +1,31 @@
 /*Write a c program to take row numbers from the user and print the following pyramid pattern.
-1
-0 1
-1 0 1
-0 1 0 1
-1 0 1 0 1
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
 */
 #include<stdio.h>
 int main()
 {
-	int row,i,p,q,j;
+	int row,spc,i,j,k;
 	printf("\nEnter the row number = ");
 	scanf("%d",&row);
 	
+	spc = row - 1;
+	
 	for(i=1;i<=row;i++)
 	{
-		if(i%2==0)
+		for(j=spc;j>=1;j--)
 		{
-			p = 0;
-			q = 1;
+			printf(" ");
 		}
-		else
+		for(k=1;k<=i;k++)
 		{
-			p = 1;
-			q = 0;
-		}
-		for(j=1;j<=i;j++)
-		{
-			if(j%2==0)
-			{
-				printf("%d ",q);
-			}
-			else
-			{
-				printf("%d ",p);
-			}
+			printf("* ");
 		}
 		printf("\n");
+		spc--;
 	}
 	
 	return 0;
